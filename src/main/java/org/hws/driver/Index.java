2846fb2e1a32eaa0f614badc73e13b3e94c583a9
Neerosoft <skylab0886@gmx.com>
2021-08-17 22:05:29 -0500

    update6 17-8-2021

--- src/main/java/org/hws/driver/Index.java
+++ src/main/java/org/hws/driver/Index.java
@@ -6,12 +6,9 @@ import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.SessionScoped;
 import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
 import javax.faces.event.ActionEvent;
-
-import org.apache.jasper.tagplugins.jstl.core.Out;
 import org.hws.dao.PanelDAO;
 import org.hws.dblink.MSSQLConnection;
 import org.pojo.Contact;
